fe3a72a0a20b270202759e248b4ed7e441f37895
Mengz You <mengz.you@gmail.com>
2015-02-15 23:21:55 +0800

    [dev] Add error html page, and adjust image files.

--- gulpfile.js
+++ gulpfile.js
@@ -52,10 +52,10 @@ gulp.task('jshint', function () {
 // Optimize Images
 gulp.task('images', function () {
   return gulp.src('src/images/**/*')
-    .pipe($.cache($.imagemin({
+    .pipe($.imagemin({
       progressive: true,
       interlaced: true
-    })))
+    }))
     .pipe(gulp.dest('dist/images'))
     .pipe($.size({title: 'images'}));
 });
